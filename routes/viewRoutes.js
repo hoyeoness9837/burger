3a7e7726a27993140ca42a630fdfa34e9a00d9fe
Hoyeon Kim <kim.hoyeon93@gmail.com>
2020-05-29 03:29:18 -0700

    updated with deployment fix

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,12 +1,15 @@
 const router = require('express').Router();
 const { Burger } = require('../controllers');
 
+router.get('/', (req, res) => {
+  Burger.getBurgers((burgers) => {
+    res.render('burgers', { burgers });
+  });
+});
 router.get('/burgers', (req, res) => {
   Burger.getBurgers((burgers) => {
     res.render('burgers', { burgers });
   });
 });
-
-
 
 module.exports = router;
